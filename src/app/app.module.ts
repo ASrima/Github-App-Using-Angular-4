8c4ac142c264959ecc5cc3734a9b34da017a1c2e
ASrima <ayeshasultanarima@gmail.com>
2018-02-17 14:39:17 -0500

    Created github profile app using anhular 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import 'rxjs/add/operator/map'; 
+import{ BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule } from '@angular/material';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
